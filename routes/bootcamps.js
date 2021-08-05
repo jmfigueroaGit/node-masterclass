f5a357ab6d6d46bac77d396af0ccf6352cee8a0b
jmfigueroaGit <jheremiah.figueroa@tup.edu.ph>
2021-08-05 15:03:04 +0800

    Photo upload done

---
+++ routes/bootcamps.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {
+	getBootcamps,
+	getBootcamp,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp,
+	getBootcampsInRadius,
+	bootcampPhotoUpload,
+} = require('../controllers/bootcamps');
+
+// Include other resourse routers
+const courseRouter = require('./courses');
+
+const router = express.Router();
+
+// Re-route into other resourse routers
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
+
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+	.route('/:id')
+	.get(getBootcamp)
+	.put(updateBootcamp)
+	.delete(deleteBootcamp);
+
+module.exports = router;
